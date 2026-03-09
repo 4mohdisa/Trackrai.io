f4f99784e5f927c0f1135a06516a07b32b51562f
4mohdisa <mohdisa233@gmail.com>
2026-03-10 01:10:39 +1030

    feat: initial commit

---
+++ app/layout.tsx
@@ -0,0 +1,20 @@
+import { LandingNavbar } from '@/components/app/landing/navbar'
+import { LandingFooter } from '@/components/app/landing/footer'
+import { Geist } from "next/font/google";
+
+const geist = Geist({subsets:['latin'],variable:'--font-sans'});
+
+
+export default function MarketingLayout({
+  children,
+}: {
+  children: React.ReactNode
+}) {
+  return (
+    <div className="min-h-screen bg-white">
+      <LandingNavbar />
+      {children}
+      <LandingFooter />
+    </div>
+  )
+}
